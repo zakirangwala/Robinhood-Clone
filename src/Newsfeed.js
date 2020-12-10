92c51a38a560592718d5ef96427dcc32817a0eef
zakirangwala <zakirangwala@gmail.com>
2020-12-10 17:40:33 -0500

    Linegraph init

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Newsfeed.css";
+import LineGraph from "./LineGraph.js";
 
 function Newsfeed() {
   return (
@@ -9,6 +10,9 @@ function Newsfeed() {
         <div className="newsfeed__portfolio">
           <h1>$114,656</h1>
           <p>+$44.63 (+0.04%) Today</p>
+        </div>
+        <div className="newsfeed__chart">
+          <LineGraph />
         </div>
       </div>
     </div>

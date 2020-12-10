9b819154019e804c6889b472853bb0a4abedf6bc
zakirangwala <zakirangwala@gmail.com>
2020-12-10 17:27:05 -0500

    create stats

---
+++ src/Stats.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Stats.css";
+
+function Stats() {
+  return (
+    <div className="stats">
+      <div className="stats__container"></div>
+    </div>
+  );
+}
+
+export default Stats;

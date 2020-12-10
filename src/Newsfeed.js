11f57c40dd9ef63fca1e312758a33983cf674c84
zakirangwala <zakirangwala@gmail.com>
2020-12-10 17:35:55 -0500

    Construct newsfeed and stats

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./Newsfeed.css";
+
 function Newsfeed() {
-  return <div></div>;
+  return (
+    <div className="newsfeed">
+      <div className="newsfeed__container"></div>
+      <div className="newsfeed__chartSection">
+        <div className="newsfeed__portfolio">
+          <h1>$114,656</h1>
+          <p>+$44.63 (+0.04%) Today</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Newsfeed;

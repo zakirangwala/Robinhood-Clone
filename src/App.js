1071fecc68a93e547f5b4069f8001a86d22ad718
zakirangwala <zakirangwala@gmail.com>
2020-12-10 17:07:30 -0500

    Create body

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header.js";
+import Newsfeed from "./Newsfeed.js";
 import "./App.css";
 
 function App() {
@@ -8,7 +9,11 @@ function App() {
       <div className="app__header">
         <Header />
       </div>
-      <div className="app__body"></div>
+      <div className="app__body">
+        <div className="aoo__container">
+          <Newsfeed />
+        </div>
+      </div>
     </div>
   );
 }

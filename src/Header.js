fbbad2a60ceae89139f32d0604d2adfd92c050d0
zakirangwala <zakirangwala@gmail.com>
2020-12-10 16:29:03 -0500

    add search icon

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import Logo from './robinhood.svg'
-import './Header.css'
+import React from "react";
+import Logo from "./robinhood.svg";
+import SearchIcon from "@material-ui/icons/Search";
+import "./Header.css";
 
 function Header() {
-    return (
-        <div className="header__wrapper">
-            <div className="header__logo">
-                <img src={Logo} width={25} alt="Robhinhood Logo"/>
-            </div>
-            <div className="header__search">
-                <div className="header__searchContainer">
-                    <input placeholder="Search" type="text" />
-                </div>
-            </div>
-            <div className="header__menuItems">
-                <a href="/">Free Stocks</a>
-                <a href="/">Portfolio</a>
-                <a href="/">Cash</a>
-                <a href="/">Messages</a>
-                <a href="/">Account</a>
-            </div>
+  return (
+    <div className="header__wrapper">
+      <div className="header__logo">
+        <img src={Logo} width={25} alt="Robhinhood Logo" />
+      </div>
+      <div className="header__search">
+        <div className="header__searchContainer">
+          <SearchIcon></SearchIcon>
+          <input placeholder="Search" type="text" />
         </div>
-    )
+      </div>
+      <div className="header__menuItems">
+        <a href="/">Free Stocks</a>
+        <a href="/">Portfolio</a>
+        <a href="/">Cash</a>
+        <a href="/">Messages</a>
+        <a href="/">Account</a>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;

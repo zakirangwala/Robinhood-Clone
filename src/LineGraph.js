9a9b86b293de94b719ea6df566dba15f3ce28372
zakirangwala <zakirangwala@gmail.com>
2020-12-12 16:02:09 -0500

    generate line graph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
 
 function LineGraph() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [graphData, setGraphData] = useState([]);
+  const data = [
+    {
+      x: 10,
+      y: 20,
+    },
+    {
+      x: 15,
+      y: 10,
+    },
+    {
+      x: 12,
+      y: 4,
+    },
+  ];
+
+  
+  return (
+    <div>
+      <Line
+        data={{
+          datasets: [
+            {
+              type: "line",
+              data: data,
+              backgroundColor: "black",
+              borderColor: "#5AC53B",
+              borderWidth: 2,
+              pointBorderColor: "rgba(0,0,0,0)",
+              pointBackgroundColor: "rgba(0,0,0,0)",
+              pointHoverBackgroundColor: "#5AC53B",
+              pointHoverBorderColor: "#000000",
+              pointHoverBorderWidth: 4,
+              pointHoverRadius: 6,
+            },
+          ],
+        }}
+        options={{
+          legend: {
+            display: false,
+          },
+          tooltips: {
+            mode: "index",
+            intersect: false,
+          },
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  display: false,
+                },
+              },
+            ],
+          },
+        }}
+      />
+    </div>
+  );
 }
 
-export default LineGraph
+export default LineGraph;

fb6e1a8cbde57e853f9ada3b5a7f205836033952
zakirangwala <zakirangwala@gmail.com>
2020-12-12 17:37:00 -0500

    console stock data

--- src/Stats.js
+++ src/Stats.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Stats.css";
 
+const BASE_URL = "https://finnhub.io/api/v1/quote/";
+const TOKEN = "bvajgj748v6ser00c70g";
+
 function Stats() {
+  const [stockData, setStockData] = useState([]);
+
+  const getStocksData = (stock) => {
+    return axios
+      .get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`)
+      .catch((error) => {
+        console.error("Error", error.message);
+      });
+  };
+
+  useEffect(() => {
+    let tempStocksData = []
+    const stocksList = [
+      "AAPL",
+      "MSFT",
+      "TSLA",
+      "FB",
+      "DASH",
+      "GOOGL",
+      "ABNB",
+      "AMZN",
+    ];
+    let promises = [];
+    stocksList.map((stock) => {
+      promises.push(
+        getStocksData(stock)
+        .then((res) => {
+          console.log(res);
+          // tempStocksData.push({
+          //   name: stock,
+          //   ...res.data
+          // });
+        })
+      )
+    });
+  }, []);
+
   return (
     <div className="stats">
       <div className="stats__container">
         <div className="stats__header">
-          <p> Stocks</p>
+          <p>Stocks</p>
         </div>
         <div className="stats__content">
           <div className="stats__rows"></div>

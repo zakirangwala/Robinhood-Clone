e2e73e963192210bdd7e696f8ff785c832b5ee7c
zakirangwala <zakirangwala@gmail.com>
2020-12-12 16:32:49 -0500

    construct skeleton containers

--- src/Stats.js
+++ src/Stats.js
@@ -4,7 +4,20 @@ import "./Stats.css";
 function Stats() {
   return (
     <div className="stats">
-      <div className="stats__container"></div>
+      <div className="stats__container">
+        <div className="stats__header">
+          <p> Stocks</p>
+        </div>
+        <div className="stats__content">
+          <div className="stats__rows"></div>
+        </div>
+        <div className="stats__header stats-lists">
+          <p>Lists</p>
+        </div>
+        <div className="stats__content">
+          <div className="stats__rows"></div>
+        </div>
+      </div>
     </div>
   );
 }

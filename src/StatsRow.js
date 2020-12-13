6c4375d6e5197cc8fc1468e76376ed5c60b34c87
zakirangwala <zakirangwala@gmail.com>
2020-12-13 13:10:20 -0500

    numeral formatting

--- src/StatsRow.js
+++ src/StatsRow.js
@@ -1,14 +1,20 @@
 import React from "react";
 import StockChart from "./stock.svg";
+import numeral from "numeral";
 import "./StatsRow.css";
 
 function StatsRow(props) {
   const percentage = ((props.price - props.openPrice) / props.openPrice) * 100;
   var sign = "";
+  var price = 0;
   if (percentage > 0) {
     sign = "+";
   }
-
+  if (props.shares !== undefined) {
+    price = props.price * props.shares;
+  } else {
+    price = props.price;
+  }
   return (
     <div className="row">
       <div className="row__intro">
@@ -19,7 +25,7 @@ function StatsRow(props) {
         <img src={StockChart} height={16} alt="" />
       </div>
       <div className="row__numbers">
-        <p className="row__price">${props.price}</p>
+        <p className="row__price">${numeral(price).format()}</p>
         <p className="row__percentage">
           {sign}
           {Number(percentage).toFixed(2)}%

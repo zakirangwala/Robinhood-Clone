9df84de98c42d500967642d504518454e800bed4
zakirangwala <zakirangwala@gmail.com>
2020-12-13 14:26:10 -0500

    initialize Timeline

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Newsfeed.css";
 import LineGraph from "./LineGraph.js";
+import Timeline from "./Timeline.js";
 
 function Newsfeed() {
   return (
@@ -13,6 +14,7 @@ function Newsfeed() {
         </div>
         <div className="newsfeed__chart">
           <LineGraph />
+          <Timeline />
         </div>
       </div>
     </div>
---
+++ src/Timeline.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Timeline.css'
+
+function Timeline() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Timeline

cff0fd6473c5ae166f89990e21eb6d53b7d59952
zakirangwala <zakirangwala@gmail.com>
2020-12-13 12:02:40 -0500

    comeplete stock wishlist -> components

--- src/StatsRow.js
+++ src/StatsRow.js
@@ -1,23 +1,29 @@
 import React from "react";
-import StockChart from "./stock.svg"
-import './StatsRow.css'
+import StockChart from "./stock.svg";
+import "./StatsRow.css";
 
 function StatsRow(props) {
+  const percentage = ((props.price - props.openPrice) / props.openPrice) * 100;
+  var sign = "";
+  if (percentage > 0) {
+    sign = "+";
+  }
+
   return (
     <div className="row">
       <div className="row__intro">
         <h1>{props.name}</h1>
-        <p>{props.shares && 
-          (props.shares + " shares")
-        }</p>
+        <p>{props.shares && props.shares + " shares"}</p>
       </div>
       <div className="row__chart">
-        <img src={StockChart} height={16} alt=""/>
+        <img src={StockChart} height={16} alt="" />
       </div>
       <div className="row__numbers">
         <p className="row__price">${props.price}</p>
-        <p className="row__percentage">200%</p>
-        {/* <p className="row__percentage"> +{Number(percentage).toFixed(2)}%</p> */}
+        <p className="row__percentage">
+          {sign}
+          {Number(percentage).toFixed(2)}%
+        </p>
       </div>
     </div>
   );

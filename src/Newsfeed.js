51b0ee3d7dad6d75590592a025449f72f90a166e
zakirangwala <zakirangwala@gmail.com>
2020-12-13 15:04:04 -0500

    styling

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -17,6 +17,10 @@ function Newsfeed() {
           <Timeline />
         </div>
       </div>
+      <div className="newsfeed__buying__section">
+        <h2> Buying Power</h2>
+        <h2> $4.11</h2>
+      </div>
     </div>
   );
 }
